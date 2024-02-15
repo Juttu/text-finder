77a3f795d13392d99121fcbbed5606b60fe3797c
Juttu Gajendra Anurag <juttuanurag@gmail.com>
2024-02-15 05:22:14 -0500

    highlight

--- textfinder.py
+++ textfinder.py
@@ -2,16 +2,21 @@ import fitz
 import os
 import json
 
-def rect_to_tuple(rect):
-    return (rect.x0, rect.y0, rect.x1, rect.y1)
-
 def find_word_in_pdf(pdf_path, word):
     doc = fitz.open(pdf_path)
     sentences = []
     locations = []
     for page_num in range(len(doc)):
         page = doc.load_page(page_num)
-        locations.extend([rect_to_tuple(rect) for rect in page.search_for(word)])
+        areas = page.search_for(word)
+        for rect in areas:
+            locations.append({
+                "page_number": page_num + 1,
+                "x0": rect.x0,
+                "y0": rect.y0,
+                "x1": rect.x1,
+                "y1": rect.y1
+            })
         text = page.get_text()
         lines = text.split('.')
         for line in lines:
@@ -19,6 +24,7 @@ def find_word_in_pdf(pdf_path, word):
                 sentences.append(line.strip())
     doc.close()
     return sentences, locations
+
 
 def process_files_in_folder(folder_path, words_list):
     files_list = os.listdir(folder_path)
@@ -33,10 +39,36 @@ def process_files_in_folder(folder_path, words_list):
 
     return result
 
+def highlight_areas(pdf_path, areas):
+    doc = fitz.open(pdf_path)
+    for page_num in range(len(doc)):
+        areas_for_page = [area for area in areas if area["page_number"] == page_num+1]
+        page = doc.load_page(page_num)
+        for area in areas_for_page:
+            rect = fitz.Rect(area["x0"], area["y0"], area["x1"], area["y1"])
+            highlight = page.add_rect_annot(rect)
+            highlight.update()
+            highlight.set_colors()
+    pdf_path.replace(".pdf", "_highlighted.pdf")
+    doc.save(pdf_path.replace(".pdf", "_highlighted.pdf").replace("pdfs/","highlighted_pdfs/"))
+    doc.close()
+
+def highlight_all_pdfs(result_json):
+    for file in result_json["files"]:
+        allLocationsList=[]
+        file_path = "pdfs/"+file["file_name"]
+        for word in file["words"]:
+            allLocationsList.extend(word["locations"])
+        highlight_areas(file_path,allLocationsList)
+            
+
 if __name__ == "__main__":
     folder_path = 'pdfs'
-    words_list = ['since', 'investment', 'capital']
-    result = process_files_in_folder(folder_path, words_list)
+    words_list = ['stones','since', 'investment', 'capital']
+    result_json = process_files_in_folder(folder_path, words_list)
 
     with open("result.json", "w") as json_file:
-        json.dump(result, json_file, indent=4, default=rect_to_tuple)
+        json.dump(result_json, json_file, indent=4)
+        
+    highlight_all_pdfs(result_json)
+
